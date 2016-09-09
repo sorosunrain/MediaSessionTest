8c25207887eeed193e91f3b313647af8f0cf296c
weipeng <weipeng@bftv.com>
2016-09-09 13:43:02 +0800

    Hello

---
+++ app/src/main/java/com/sunrain/mediasessiontest/activitys/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sunrain.mediasessiontest.activitys;
+
+import android.os.Bundle;
+
+import com.sunrain.mediasessiontest.R;
+import com.sunrain.mediasessiontest.activitys.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
